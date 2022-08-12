162c520563f4b5263ebcdbe790972d8d295efb85
kaifsindhi <kaifsindhI@gmail.com>
2022-08-12 11:50:58 -0500

    styling to create a list

--- frontend/src/components/board/ListTileAdd.jsx
+++ frontend/src/components/board/ListTileAdd.jsx
@@ -1,15 +1,49 @@
-import React from "react";
+import { useState } from "react";
 import { AiOutlinePlus } from "react-icons/ai";
+import { MdOutlineCancel } from "react-icons/md";
 
 const ListTileAdd = () => {
-  return (
-    <div>
+  const [action, setAction] = useState("");
+  // actions : Create/Delete/Edit
+
+  const renderCreateDefault = () => {
+    return (
       <div className="list">
-        <div className="add_list">
+        <div className="add_list" onClick={() => setAction("create")}>
           <AiOutlinePlus size={18} color="white" />
           <h6>Add a list</h6>
         </div>
       </div>
+    );
+  };
+
+  const renderCreateList = () => {
+    return (
+      <form>
+        <div className="list">
+          <div className="enter_list_title">
+            <input
+              type="text"
+              id="Title"
+              placeholder="Enter a list title..."
+            ></input>
+          </div>
+          <div className="enter_list_title">
+            <button>Add list</button>
+            <button className="cancel" onClick={() => setAction("")}>
+              <MdOutlineCancel color="inherit" size={"100%"} />
+            </button>
+          </div>
+          <div className="list_end_pad" />
+        </div>
+      </form>
+    );
+  };
+
+  return (
+    <div>
+      {action === "" && renderCreateDefault()}
+      {action === "create" && renderCreateList()}
     </div>
   );
 };
