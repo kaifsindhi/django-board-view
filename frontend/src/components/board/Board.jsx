3fd779e3d19b2017f12ffca98e838aa9dc16e16e
kaifsindhi <kaifsindhI@gmail.com>
2022-08-13 16:43:05 -0500

    edit board title

--- frontend/src/components/board/Board.jsx
+++ frontend/src/components/board/Board.jsx
@@ -1,11 +1,11 @@
 import { useEffect, useState, useRef } from "react";
 import { useParams } from "react-router-dom";
 import List from "./List";
-import TopBar from "./TopBar";
 import ListTileAdd from "./AddAListTile";
 import "./board.css";
 
 const Board = () => {
+  const [action, setAction] = useState("");
   const params = useParams();
 
   const [board, setBoard] = useState([]); // Board info
@@ -25,9 +25,58 @@ const Board = () => {
       .then(setLists);
   }, [lists]);
 
+  const editBoard = () => {
+    console.log(board);
+
+    const requestOptions = {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        Id: board.Id,
+        Title: board.Title,
+      }),
+    };
+    fetch("http://127.0.0.1:8000/board/" + board.Id, requestOptions).then(
+      (response) => {
+        console.log(response.json());
+      }
+    );
+  };
+
+  const renderCreateDefault = () => {
+    return (
+      <button className="title" onClick={() => setAction("edit")}>
+        <h5>{board.Title}</h5>
+      </button>
+    );
+  };
+
+  const renderEditInput = () => {
+    return (
+      <form
+        onSubmit={() => {
+          setAction("");
+          editBoard();
+        }}
+      >
+        <input
+          type="text"
+          id="Title"
+          value={board.Title}
+          onChange={(e) =>
+            setBoard({ Id: board.Id, Title: e.target.value })
+          }
+        />
+      </form>
+    );
+  };
+
   return (
     <section className="board">
-      <TopBar Title={board.Title} />
+      <div className="top">
+        {action === "" && renderCreateDefault()}
+        {action === "edit" && renderEditInput()}
+      </div>
       <div className="board_flex">
         {lists.map((list) => (
           <List board_id={params.board_id} list={list} />
--- frontend/src/components/board/TopBar.jsx
+++ frontend/src/components/board/TopBar.jsx
@@ -1,13 +1,55 @@
-import React from "react";
+import { useState } from "react";
 import { AiOutlineStar, AiFillStar, AiOutlineLock } from "react-icons/ai";
-import {MdPublic} from 'react-icons/md';
+import { MdPublic } from "react-icons/md";
 
 const TopBar = (props) => {
+  const [action, setAction] = useState("");
+  const [b, setBoardTitle] = useState(props.board);
+
+  const editBoard = () => {
+    console.log(b);
+
+    const requestOptions = {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        Id: b.Id,
+        Title: b.Title,
+      }),
+    };
+    fetch("http://127.0.0.1:8000/board/" + b.Id, requestOptions).then(
+      (response) => {
+        console.log(response.json());
+      }
+    );
+  };
+
+  const renderCreateDefault = () => {
+    return (
+      <button className="title" onClick={() => setAction("edit")}>
+        <h5>{props.board.Title}</h5>
+      </button>
+    );
+  };
+
+  const renderEditInput = () => {
+    return (
+      <form
+        onSubmit={() => {
+          setAction("");
+          editBoard();
+        }}
+      >
+        <input />
+      </form>
+    );
+  };
+
   return (
     <div className="top">
-      <button className="title">
-        <h5>{props.Title}</h5>
-      </button>
+      {action === "" && renderCreateDefault()}
+      {action === "edit" && renderEditInput()}
+
       {/* <button className="icon dark">
         <div className="star">
           <AiOutlineStar color="inherit" size={20} />
@@ -28,3 +70,4 @@ const TopBar = (props) => {
 };
 
 export default TopBar;
+//defunct
